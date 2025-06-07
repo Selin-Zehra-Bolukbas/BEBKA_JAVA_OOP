80889c9a332a2bebdb1351aebafa5acb1206ed1f
PC <selinz.bolukbas@gmail.com>
2025-06-07 20:29:21 +0300

    [JA-1] - Sınıf ve Nesne Oluşturma

---
+++ core_java/src/main/java/com/selin/Sinif_ve_Nesne_Olusturma/Car.java
@@ -0,0 +1,12 @@
+package com.selin.Sinif_ve_Nesne_Olusturma;
+
+public class Car {
+    public String brand;
+    public String model;
+    public String year;
+
+    public void yazdir() {
+        System.out.println("" + brand + " " + model + " " + year);
+    }
+}
+
